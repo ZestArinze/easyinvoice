0a2e313911fa3ea77875e81014b4518b4cb02ad2
Arinze Anakor <arinzeanakor@gmail.com>
2021-06-10 14:15:28 +0100

    chore(setup): create project ui template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { useEffect } from 'react';
-import {
-  Switch,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 
 import './css/style.scss';
 
@@ -12,23 +8,26 @@ import './charts/ChartjsConfig';
 
 // Import pages
 import Dashboard from './pages/Dashboard';
+import AddInvoice from './pages/AddInvoice';
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector('html').style.scrollBehavior = 'auto';
+    window.scroll({ top: 0 });
+    document.querySelector('html').style.scrollBehavior = '';
     focusHandling('outline');
   }, [location.pathname]); // triggered on route change
 
   return (
     <>
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <Dashboard />
+        </Route>
+        <Route exact path='/add-invoice'>
+          <AddInvoice />
         </Route>
       </Switch>
     </>
